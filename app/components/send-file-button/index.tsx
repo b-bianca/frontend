ab117e3b3b072b3b12732dbd3dc6f736b5b16cdc
b-bianca <bianca.cms@outlook.com.br>
2023-10-06 07:49:59 -0300

    update component

--- app/components/send-file-button/index.tsx
+++ app/components/send-file-button/index.tsx
@@ -6,8 +6,9 @@ export default function SendFileButton ({selectedFile, handleUpload} : Props) {
     return <button 
     disabled={!selectedFile} 
     onClick={handleUpload} 
-    className={`${selectedFile ? 'bg-stone-50' : 'bg-neutral-800'} flex items-center rounded-full pt-3 pb-3 pl-5 pr-5 text-stone-700`}>
-    Send
+    className={`${selectedFile ? 'bg-stone-50' : 'bg-neutral-800'} flex items-center rounded-full pt-5 pb-3 pl-5 pr-5 text-stone-700`}
+    style={{ margin: '0 auto' }}>
+    Send   
       <FontAwesomeIcon
       className='cursor-pointer ml-1'
       size='1x'
